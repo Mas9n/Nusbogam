d750018915ba9be30bd8f1264d071c83afa770af
mas9n <maks_korp@mail.ru>
2016-09-17 22:27:44 +0300

    start

---
+++ app/src/main/java/com/example/maks/nusbogam/Perviysin.java
@@ -0,0 +1,13 @@
+package com.example.maks.nusbogam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Perviysin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perviysin);
+    }
+}
